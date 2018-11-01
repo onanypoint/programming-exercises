328a15f9ebd191ca6943233a7dfda672f3d2282e
onanypoint <onanypoint@gmail.com>
2018-11-01 11:39:59 +0100

    Refactor helper functions AOC

---
+++ advent-of-code/helpers/functions.py
@@ -0,0 +1,29 @@
+import itertools
+
+def read_file(dir, file):
+    return open(dir + file,'r').readlines()
+
+def load(dir, day):
+    return read_file(dir, "day{}.txt".format(day))
+    
+def mapl(fn, *args):
+    return list(map(fn, *args))
+
+def first(l): 
+    return next(iter(l))
+
+def is_unique(l):
+    return len(set(l)) == len(l)
+
+def is_prime(n):
+    return n > 1 and all(n%i for i in itertools.islice(itertools.count(2), int(math.sqrt(n)-1)))
+
+def sort(items):
+    cast = ''.join if isinstance(items, str) else tuple 
+    return cast(sorted(items))
+
+def add(x,y):
+    return map(sum, zip(x,y))
+
+def mult(x, v):
+	return [i*v for i in x]
